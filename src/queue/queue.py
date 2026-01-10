64e1fc13d0b5cd60176d09ec3c77cbd3663b90f7
Brendan Saliba <brendanbsaliba@gmail.com>
2026-01-10 11:57:17 -0700

    added dequeue to the queue class

--- src/queue/queue.py
+++ src/queue/queue.py
@@ -1,14 +1,20 @@
 class Node():
     def __init__(self, data=None, next=None, prev=None):
         self.data = data
-        self.next = next
-        self.prev = prev
+        self.next: Node = next
+        self.prev: Node = prev
 
 class Queue():
     def __init__(self):
-        self.head = None
-        self.tail = None
-        self.size = 0
+        self.head: Node = None
+        self.tail: Node = None
+        self.size: int = 0
+
+    def iter(self):
+        current = self.head
+        while current is not None:
+            yield current
+            current = current.next
 
     def enqueue(self, data):
         n = Node(data=data)
@@ -20,3 +26,24 @@ class Queue():
             self.tail.next = n
             self.tail = n
         self.size += 1
+
+    def dequeue(self):
+        if self.size == 1:
+            self.head = None
+            self.tail = None
+            self.size -= 1
+        elif self.size > 1:
+            self.head = self.head.next
+            self.head.prev = None
+            self.size -= 1
+        else:
+            print("Queue is empty")
+
+if __name__ == "__main__":
+    q = Queue()
+    q.enqueue("item 1")
+    q.enqueue("item 2")
+    q.enqueue("item 3")
+  
+    for n in q.iter():
+        print(n.data)
